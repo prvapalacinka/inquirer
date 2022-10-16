8072488ffde6cdff71d25956bea0145368bcd909
Broly <vidovicp20@gmail.com>
2022-10-16 13:49:05 -0400

    ReadMe sections, linking added new user inputs

--- Develop/index.js
+++ Develop/index.js
@@ -1,36 +1,63 @@
 // TODO: Include packages needed for this application
-const inquirer = require ('inquirer');
-const generateMarkdown = require ('./utils/generateMarkdown');
-
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+var fs = require('fs');
 inquirer
   .prompt([
-   { type: 'input',
-    name: 'title',
-    message: 'What is the name of your project/repository?',
-    default: 'Test'
-   },
-   {
-    type: 'list',
-    name: 'license',
-    message: 'Which of these?',
-    choices: ['MIT', 'Apache', 'Unlicense'],
-    default: 'One'
-   },
-   {
-    type: 'input',
-    name: 'description',
-    message: 'Describe your application',
-   },
-
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of your project/repository?',
+      default: 'Test'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which of these?',
+      choices: ['MIT', 'Apache', 'Unlicense'],
+      default: 'One'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your application',
+    },
+    {
+      type: 'input',
+      name: 'description_challenges',
+      message: 'What are some challenges you were presented with while developing your application?',
+    },
+    {
+      type: 'input',
+      name: 'description_installation',
+      message: 'How can a user install and use the application?',
+    },
+    {
+      type: 'input',
+      name: 'dependencies',
+      message: 'Please list any dependencies within your project or write, "No dependencies":',
+    },
+    {
+      type: 'input',
+      name: 'credentials',
+      message: 'Please write any credentials that may be required for your app:',
+    },
+    {
+      type: 'input',
+      name: 'contribute',
+      message: 'Please write the guidelines for contribution:',
+    },
   ])
   .then((answers) => {
-   console.log(generateMarkdown(answers));
-   // writeToFile("NEWREADME.md", generateMarkdown(answers))
+    console.log(generateMarkdown(answers));
+    // writeToFile("NEWREADME.md", generateMarkdown(answers))
   })
   .catch((error) => {
     if (error.isTtyError) {
+      prompt(["Couldn't be rendered in the current environment"]);
       // Prompt couldn't be rendered in the current environment
     } else {
+      prompt(["Something else went wrong"]);
       // Something else went wrong
     }
   });
@@ -39,12 +66,14 @@ const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  // run an fs command to write the file using the above parameters
-  //fs.writeFile(fileName, data, (err) => console.log(err))
-}
+  fs.writeFile('NEWREADME.md', generateMarkdown(answers))
+};
+// run an fs command to write the file using the above parameters
+//fs.writeFile(fileName, data, (err) => console.log(err))
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { default: inquirer } = require("inquirer");
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if ... https://www.mit.edu/~amini/LICENSE.md
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,12 +19,23 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
   ## TOC
-  
+  [Description](https://www.example.com)
+  [License](https://www.example.com)
+  [Installation](https://www.example.com)
+  [Contribute](https://www.example.com)
   ## Description
   ${data.description}
+  ${data.description_challenges}
   ## License
   ${data.license}
 
+  ##Installation
+  ${data.description_installation}
+  ${data.dependencies}
+  ${data.credentials}
+
+  ##Contribute
+  ${data.contribute}
 `;
 }
 
